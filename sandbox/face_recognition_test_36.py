34190d42aded23b1a6258df4585a0034c583510c
Jason Smalridge <jason.smalridge@autodesk.com>
2018-07-20 08:29:27 -0400

    resize window to just face

--- sandbox/face_recognition_test_36.py
+++ sandbox/face_recognition_test_36.py
@@ -10,6 +10,7 @@ eyeCascade = cv2.CascadeClassifier(eyeCascPath)
 video_capture = cv2.VideoCapture(0)
 
 while True:
+    
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
@@ -17,14 +18,18 @@ while True:
 
     faces = faceCascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
+        roi = []
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         eyes = eyeCascade.detectMultiScale(roi_gray)
         ex,ey,ew,eh = eyes[0]
         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        roi = frame[y:y+h, x:x+w]
+        roi_resized = cv2.resize(roi, (0,0), fx=2, fy=2)
 
+    
     # Display the resulting frame
-    cv2.imshow('Video', frame)
+    cv2.imshow('Video', roi_resized)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
